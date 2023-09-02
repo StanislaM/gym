3593e5f12bb500fd0872bad7c5afb9d13a535eb5
Mokhoid Stanislav <mohoidstanislav@gmail.com>
2023-09-02 22:37:00 +0300

    added home page

--- src/shared/ActionButton.tsx
+++ src/shared/ActionButton.tsx
@@ -9,7 +9,7 @@ type Props = {
 const ActionButton = ({ children, setSelectedPage }: Props) => {
     return (
         <AnchorLink
-            className="rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 transition duration-300 cursor-pointer"
+            className="rounded-md font-bold bg-secondary-500 px-10 py-2 hover:bg-primary-500 transition duration-300 cursor-pointer"
             onClick={() => setSelectedPage(SelectedPage.ContactUs)}
             href={`#${SelectedPage.ContactUs}`}
         >
